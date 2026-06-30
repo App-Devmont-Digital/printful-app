3d766d003838087dc3439afae9f5279446b76a28
mustafa695 <amustufa37@gmail.com>
2026-06-30 12:18:18 +0500

    mongodb configs and connections

--- app/shopify.server.js
+++ app/shopify.server.js
@@ -5,7 +5,12 @@ import {
   shopifyApp,
 } from "@shopify/shopify-app-react-router/server";
 import { PrismaSessionStorage } from "@shopify/shopify-app-session-storage-prisma";
+import { MongoDBSessionStorage } from "@shopify/shopify-app-session-storage-mongodb";
 import prisma from "./db.server";
+
+
+const mongoUrl = process.env.DATABASE_URL;
+const mongoDbName = process.env.DATABASE_NAME;
 
 const shopify = shopifyApp({
   apiKey: process.env.SHOPIFY_API_KEY,
@@ -14,7 +19,8 @@ const shopify = shopifyApp({
   scopes: process.env.SCOPES?.split(","),
   appUrl: process.env.SHOPIFY_APP_URL || "",
   authPathPrefix: "/auth",
-  sessionStorage: new PrismaSessionStorage(prisma),
+  // sessionStorage: new PrismaSessionStorage(prisma),
+   sessionStorage: new MongoDBSessionStorage(mongoUrl, mongoDbName),
   distribution: AppDistribution.AppStore,
   future: {
     expiringOfflineAccessTokens: true,
